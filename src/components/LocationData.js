ce1a09f94a46f1a4430a7cc0dfa6059dc747260e
alhirz <ic16m007@technikum-wien.at>
2018-08-11 21:56:34 +0200

    gps fixes

--- src/components/LocationData.js
+++ src/components/LocationData.js
@@ -4,10 +4,10 @@ import { DeviceEventEmitter } from 'react-native'
 
 class LocationData extends Component {
 	//intervalId;
-	watchId;
 	// if TRUE => GPS
 	// if FALSE => WIFI
-	useGps = true;
+	//useGps = false;
+	//initGPS = true
 
 	constructor(props) {
 		super(props);
@@ -24,10 +24,12 @@ class LocationData extends Component {
 	}
 
 	componentDidMount() {
-		if (this.props.recording) {
+		/* if (this.props.recording) {
+			console.log("component mounted --> execute loadGPSData")
 			this.loadGPSData();
 			//this.setInterval();
-		}
+		} */
+
 	}
 
 	setInterval() {
@@ -36,9 +38,54 @@ class LocationData extends Component {
 		}, 10000);
 	}
 
-	// ISSUE --> first two entries still the same 
 	loadGPSData() {
 		console.log("loading GPS data...")
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				var { accuracy, heading, latitude, longitude, speed } = position.coords;
+				const recordData = {
+					'accuracy': accuracy,
+					'heading': heading,
+					'latitude': latitude,
+					'longitude': longitude,
+					'speed': speed,
+					'timestamp': position.timestamp
+				}
+
+				this.setState({ ...recordData, error: null });
+				this.props.onUpdate("GPS", recordData)
+				//this.watchGPSData()
+				console.log(recordData)
+				//alert(JSON.stringify(recordData))
+			},
+			(error) => {
+				this.setState({
+					accuracy: "",
+					// altidude: "",
+					heading: "",
+					latitude: "",
+					longitude: "",
+					speed: "",
+					timestamp: "",
+					error: error.message,
+				})
+				//alert(JSON.stringify(error))
+			},
+			{
+				enableHighAccuracy: false,
+				timeout: 10000,
+				maximumAge: 1000
+			}
+		);
+		console.log("done loading GPS")
+		console.log(this.state.error)
+	}
+
+	// ISSUE --> first two entries still the same 
+	watchGPSData() {
+		//if(this.watchId) navigator.geolocation.clearWatch(this.watchId)
+		//alert(JSON.stringify("watching GPS data..."))
+		console.log("watching GPS data...")
 		this.watchId = navigator.geolocation.watchPosition(
 			(position) => {
 				var { accuracy, heading, latitude, longitude, speed } = position.coords;
@@ -54,6 +101,9 @@ class LocationData extends Component {
 				this.setState({ ...recordData, error: null });
 				this.props.onUpdate("GPS", recordData)
 				console.log(recordData)
+				//console.log(typeof(position.timestamp))
+				//console.log(position.coords)
+				//alert(JSON.stringify(recordData))
 			},
 			(error) => {
 				this.setState({
@@ -67,27 +117,44 @@ class LocationData extends Component {
 					error: error.message,
 				})
 				// use WiFi if GPS is not available after first time
-				this.useGps = false;
+				//this.useGps = false;
+				console.log("error")
+				console.log(this.state.error)
 			},
 			{
-				enableHighAccuracy: this.useGps,
-				timeout: 10000,
+				enableHighAccuracy: true,
+				timeout: 5000,
 				maximumAge: 1000
+				//distanceFilter: 10
 			}
 		);
+		console.log(this.watchId)
+		//this.useGps = true
 	}
 
 
 	componentDidUpdate(prevProps) {
 		const { recording } = this.props;
-
+		//console.log(this.useGps)
 		if (!recording && prevProps.recording) {
+			console.log("clearWatch will be executed")
 			navigator.geolocation.clearWatch(this.watchId)
 			//clearInterval(this.intervalId);
-		} else if (recording && !prevProps.recording) {
-			this.loadGPSData();
+		} 
+		else if (recording && !prevProps.recording) {
+			//if (this.initGPS) {
+			//	this.initGPS = false
+			//	this.loadGPSData()
+			//}
+			//else {
+				//setTimeout(this.watchGPSData(), 10000)
+			//	console.log("watchGPS() will be executed")
+				this.watchGPSData();
+			//}
+			
 			//this.setInterval();
 		}
+		
 	}
 
 	render() {
